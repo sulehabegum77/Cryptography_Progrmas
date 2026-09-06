aedb5d9ef1610d2d640b45b6e0b17007e93a9e95
sulehabegum77 <sulehabegum77@gmail.com>
2026-09-06 11:12:49 +0530

    Implement play fair cipher for encryption and decryption

--- Playfair Cipher.py
+++ Playfair Cipher.py
@@ -2,46 +2,70 @@
 
 m = ["MONAR", "CHYBD", "EFGIK", "LPQST", "UVWXZ"]
 
-text = input("Enter a text: ").upper()
+text = input("Enter a text: ").upper().replace(" ", "")
+text = text.replace("J", "I")
+
+# Add X if text has odd number of letters
+if len(text) % 2 != 0:
+    text = text + "X"
 
 def pos(ch):
-    if ch == 'J':
-        ch = 'I'
     for i in range(5):
         for j in range(5):
             if m[i][j] == ch:
                 return i, j
 
-a, b = text[0], text[1]
-r1, c1 = pos(a)
-r2, c2 = pos(b)
+encrypted = ""
 
 # Encryption
-if r1 == r2:
-    e1 = m[r1][(c1+1)%5]
-    e2 = m[r2][(c2+1)%5]
-elif c1 == c2:
-    e1 = m[(r1+1)%5][c1]
-    e2 = m[(r2+1)%5][c2]
-else:
-    e1 = m[r1][c2]
-    e2 = m[r2][c1]
+for i in range(0, len(text), 2):
 
-encrypted = e1 + e2
+    a = text[i]
+    b = text[i + 1]
+
+    r1, c1 = pos(a)
+    r2, c2 = pos(b)
+
+    if r1 == r2:
+        e1 = m[r1][(c1 + 1) % 5]
+        e2 = m[r2][(c2 + 1) % 5]
+
+    elif c1 == c2:
+        e1 = m[(r1 + 1) % 5][c1]
+        e2 = m[(r2 + 1) % 5][c2]
+
+    else:
+        e1 = m[r1][c2]
+        e2 = m[r2][c1]
+
+    encrypted = encrypted + e1 + e2
+
 print("Encrypted:", encrypted)
 
+
 # Decryption
-r1, c1 = pos(e1)
-r2, c2 = pos(e2)
+decrypted = ""
 
-if r1 == r2:
-    d1 = m[r1][(c1-1)%5]
-    d2 = m[r2][(c2-1)%5]
-elif c1 == c2:
-    d1 = m[(r1-1)%5][c1]
-    d2 = m[(r2-1)%5][c2]
-else:
-    d1 = m[r1][c2]
-    d2 = m[r2][c1]
+for i in range(0, len(encrypted), 2):
 
-print("Decrypted:", d1 + d2)
+    a = encrypted[i]
+    b = encrypted[i + 1]
+
+    r1, c1 = pos(a)
+    r2, c2 = pos(b)
+
+    if r1 == r2:
+        d1 = m[r1][(c1 - 1) % 5]
+        d2 = m[r2][(c2 - 1) % 5]
+
+    elif c1 == c2:
+        d1 = m[(r1 - 1) % 5][c1]
+        d2 = m[(r2 - 1) % 5][c2]
+
+    else:
+        d1 = m[r1][c2]
+        d2 = m[r2][c1]
+
+    decrypted = decrypted + d1 + d2
+
+print("Decrypted:", decrypted)
